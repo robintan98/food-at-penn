f81e8c75bdd2563d2a5295480ee7f3172ac3ce8e
robintan98 <46473314+robintan98@users.noreply.github.com>
2020-03-09 18:01:29 -0400

    Created back-end for accounts page using mongo

--- nodeproject1/routes/index.js
+++ nodeproject1/routes/index.js
@@ -14,4 +14,20 @@ router.get('/register', function(req, res) {
   res.render('register', { title: 'Register'});
 });
 
+/* GET accounts page.
+  Router to accounts page, where admins can see currently registered accounts
+  Note: From non-admin end, should not be able to see current accounts, mainly for
+  development purposes
+  Extracts db object from request, fill docs variable with accounts data, do page render
+*/
+router.get('/accounts', function(req, res) {
+  var db = req.db;
+  var collection = db.get('accountscollection');
+  collection.find({}, {}, function(e, docs) {
+    res.render('accounts', {
+      "accounts" : docs
+    });
+  });
+});
+
 module.exports = router;
