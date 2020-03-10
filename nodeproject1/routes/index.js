31a5b0df34cd022b8135a29cc37d16d3947df8b9
robintan98 <46473314+robintan98@users.noreply.github.com>
2020-03-10 02:20:08 -0400

    created login page, login form

--- nodeproject1/routes/index.js
+++ nodeproject1/routes/index.js
@@ -14,6 +14,14 @@ router.get('/register', function(req, res) {
   res.render('register', { title: 'Register'});
 });
 
+/* GET login page.
+  Router to register page, where users can register accounts
+  @developer: Robin
+*/
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Login'});
+});
+
 /* GET accounts page.
   Router to accounts page, where admins can see currently registered accounts
   Note: From non-admin end, should not be able to see current accounts, mainly for
@@ -32,7 +40,7 @@ router.get('/accounts', function(req, res) {
 });
 
 /* POST to Register
-  Routes user to homepage after registering an account
+  Routes user to dashboard after registering an account
   Also inserts account information into MongoDB
   Note: May need to create the 'accountscollection' db beforehand for dev purposes 
   @developer: Robin
@@ -52,7 +60,7 @@ router.post('/register', function(req, res) {
 
   // Check DB for repeated usernames
   collection.find().then(collection => {
-    collection.forEach(function(account){
+    collection.forEach(function(account) {
         // If account's username already exists, redirect to error code
         if (account.username == username) {
           res.send("Account already exists");
@@ -72,10 +80,47 @@ router.post('/register', function(req, res) {
           res.send("There was a problem adding the information to the database.");
       }
       else {
-          // Else, redirect to homepage
+          // Else, redirect to dashboard
           res.redirect("accounts");
       }
   });
+
+});
+
+/* POST to Login
+  Routes user to dashboard after validating username and password
+  Redirects to error code if username and password do not match
+  Redirects to login if no username is found
+  Note: May need to create the 'accountscollection' db beforehand for dev purposes 
+  @developer: Robin
+*/
+router.post('/login', function(req, res) {
+
+  var db = req.db;
+
+  // Attributes = attributes from req.body
+  var username = req.body.username;
+  var password = req.body.password;
+
+  // Collection name is 'accountscollection'
+  var collection = db.get('accountscollection');
+
+  // Check DB to validate username and password
+  collection.find().then(collection => {
+    collection.forEach(function(account) {
+        if (account.username == username) {
+          // If username and password does not match with account's password, redirect to error code
+          if (account.password != password) {
+            res.send("Password is incorrect!");
+          } else {
+            // Redirect to dashboard if username and password match
+            res.redirect("accounts");
+          }
+        }
+    });
+    // If no username in the collection matches, then redirect to login page
+    res.redirect("login");
+  })
 
 });
 
