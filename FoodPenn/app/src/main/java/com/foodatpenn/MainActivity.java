641699bae836f986b6390f8fa07de95071d5ccb5
KRagula <k.ragula1@gmail.com>
2020-04-17 23:46:22 -0600

    Added the login server with nodelogin and basic name, email, and password features.  Not yet integrated into android

--- FoodPenn/app/src/main/java/com/foodatpenn/MainActivity.java
+++ FoodPenn/app/src/main/java/com/foodatpenn/MainActivity.java
@@ -4,14 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.Button;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.foodpenn.R;
+import com.foodatpenn.Retrofit.IMyService;
+import com.foodatpenn.Retrofit.RetrofitClient;
 import com.foodatpenn.data.RegistrationStore;
 import com.foodatpenn.data.RegisterStoreDataLocal;
+
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
     private EditText userEmail;
@@ -19,6 +30,16 @@ public class MainActivity extends AppCompatActivity {
     private Button login;
     private Button createAcct;
     private RegistrationStore registered;
+
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
+    IMyService iMyService;
+
+    @Override
+    protected void onStop() {
+        compositeDisposable.clear();
+        super.onStop();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +51,12 @@ public class MainActivity extends AppCompatActivity {
         login = (Button) findViewById(R.id.login);
         createAcct = (Button) findViewById(R.id.createAcct);
         registered = RegisterStoreDataLocal.getInstance();
+
+
+        //Init service
+        Retrofit retrofitClient = RetrofitClient.getInstance();
+        iMyService = retrofitClient.create(IMyService.class);
+
     }
 
     public void loginAttempt(View view) {
@@ -41,15 +68,26 @@ public class MainActivity extends AppCompatActivity {
             i.putExtra("Email", userName);
             startActivityForResult(i, 2);
         } else {
-            Toast.makeText(
-                    this,
-                    "Username or password incorrect",
-                    Toast.LENGTH_LONG
-            ).show();
+//            Toast.makeText(
+//                    this,
+//                    "Username or password incorrect",
+//                    Toast.LENGTH_LONG
+//            ).show();
         }
     }
 
     private boolean validate (String userName, String passString) {
+        Log.v("email:", userName);
+        Log.v("password", passString);
+        compositeDisposable.add(iMyService.loginUser(userName, passString)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>(){
+                    @Override
+                    public void accept(String response) throws Exception {
+                        Toast.makeText(MainActivity.this, ""+response, Toast.LENGTH_LONG).show();
+                    }
+                }));
         return registered.verifyLogin(userName, passString);
     }
 
